d339e32d3afd90b70c00208dbf8a0c3acc7bdb96
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-08-21 10:05:21 -0600

    operations

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+tasksToDoEl;
+
+var createTaskHandler = function() {
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  }
+
+buttonEl.addEventListener("click", createTaskHandler); {
+   
+  };
+var btn = window.document.querySelector("button");
+
+tasksToDoEl.appendChild(taskItemEl);
+taskItemEl.className = "task-item";
+
+
+document.querySelector("#save-task");
+document.querySelector("button").textContent;
+console.dir(btn);
