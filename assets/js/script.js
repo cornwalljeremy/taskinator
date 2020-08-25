b04f54ad3dcaba3d4912c6ab9155b0bf61bf0336
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-08-24 19:45:19 -0600

    event-prevent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,26 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
-tasksToDoEl;
-var createTaskHandler = function() {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  }
+var btn = window.document.querySelector("button");
 
-buttonEl.addEventListener("click", createTaskHandler); {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-  };
+var createTaskHandler = function (event) {
+  
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
 
-  var createTaskHandler = function() {
-    // var listItemEl = document.createElement("li");
-    // listItemEl.className = "task-item";
-    // listItemEl.textContent = "This is a new task.";
-    // tasksToDoEl.appendChild(listItemEl);
-  }
+  event.preventDefault();
+};
 
 var taskItemEl = document.createElement("li");
 taskItemEl.textContent = "hello";
 
-var btn = window.document.querySelector("button");
 
-tasksToDoEl.appendChild(taskItemEl);
+
+// tasksToDoEl.appendChild(taskItemEl);
 taskItemEl.className = "task-item";
-
 
 document.querySelector("#save-task");
 document.querySelector("button").textContent;
-console.dir(btn);
+formEl.addEventListener("submit", createTaskHandler);
+// console.dir(btn);
